d06db5da4393d4b4e71430f139dcb1e2d82b4e9a
Ryan Yan <ryan.yan@kcl.ac.uk>
2022-10-09 12:46:57 +0100

    refined Thing model

--- things/models.py
+++ things/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(AbstractUser):
-    name = models.CharField(max_length = 30)
-    description = models.CharField(max_length = 120)
-    quantity = models.CharField(max_length = 3)
+    name = models.CharField(max_length = 30, unique = True, blank = False)
+    description = models.CharField(max_length = 120, unique = False, blank = True)
+    quantity = models.IntegerField(
+    unique = False,
+    validators = [MinValueValidator(0), MaxValueValidator(100)]
+    )
