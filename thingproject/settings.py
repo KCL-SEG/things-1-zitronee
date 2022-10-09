52ff02307f8f8c2cb5937925daca5483182ec51d
Ryan Yan <ryan.yan@kcl.ac.uk>
2022-10-09 12:36:56 +0100

    Added basic things model

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# THIS WILL TELL DJANGO THAT THIS IS OUR NEW USER MODEL for authentication purposes.
+AUTH_USER_MODEL = 'things.Thing'
--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class Thing(AbstractUser):
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 120)
+    quantity = models.CharField(max_length = 3)
