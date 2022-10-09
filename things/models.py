fbce1ccedae5fefbdec6f18927010ca27c6ff518
Ryan Yan <ryan.yan@kcl.ac.uk>
2022-10-09 13:54:36 +0100

    Fixed Thing class

--- things/models.py
+++ things/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 from django.core.validators import MinValueValidator
 
-class Thing(models.Model):
+class Thing (models.Model):
     name = models.CharField(max_length = 30, unique = True, blank = False)
     description = models.CharField(max_length = 120, unique = False, blank = True)
     quantity = models.IntegerField(
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Thing
+from django.core.exceptions import ValidationError
+class ThingModelTestCase(TestCase):
+
+    def test_Construction(self):
+        self.thing = Thing(
+            name = 'Apple',
+            description = 'A red apple',
+            quantity = 10
+            #self.thing.full_clean()
+        )
+
+
+    def _assert_model_is_valid(self):
+        try:
+            #Checks if user is valid for DB or not.
+            self.thing.full_clean()
+
+        except (ValidationError):
+            self.fail('Test model should be valid')
+
+            '''checks if user is invalid for DB'''
+    def _assert_model_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.thing.full_clean()
+'''
+    def test_valid_thing(self):
+        self._assert_model_is_valid()'''
